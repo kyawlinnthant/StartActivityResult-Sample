942b88ab6dcd744667e40522635cd37fb5db5b76
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2021-11-19 10:35:35 +0630

    passing data with navArgs

--- app/src/main/java/com/galaxytechno/activityresultcode/A.kt
+++ app/src/main/java/com/galaxytechno/activityresultcode/A.kt
@@ -1,14 +1,16 @@
 package com.galaxytechno.activityresultcode
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
-class A : AppCompatActivity() {
+class A : Fragment() {
 
     private lateinit var btnOld: Button
     private lateinit var tvOld: TextView
@@ -16,59 +18,37 @@ class A : AppCompatActivity() {
     private lateinit var tvNew: TextView
 
 
-    companion object {
-        const val CODE = 100
-        const val EXTRA_KEY = "com.fill.name"
+    private val args: AArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_a)
-        btnOld = findViewById(R.id.btn_old_a)
-        tvOld = findViewById(R.id.tv_old_a)
-        btnNew = findViewById(R.id.btn_new_a)
-        tvNew = findViewById(R.id.tv_new_a)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+    }
+
+    private fun init(view: View) {
+        btnOld = view.findViewById(R.id.btn_old_a)
+        tvOld = view.findViewById(R.id.tv_old_a)
+        btnNew = view.findViewById(R.id.btn_new_a)
+        tvNew = view.findViewById(R.id.tv_new_a)
 
         btnOld.setOnClickListener {
-            fetchResultFromB()
+            findNavController().navigate(R.id.action_a_to_b)
         }
         btnNew.setOnClickListener {
-            fetchResultFromBWithNewStrategy()
-        }
-    }
-
-
-    private val resultFromB =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-
-            it?.let {
-                if (it.resultCode == Activity.RESULT_OK) {
-
-                    tvNew.text = it.data!!.getStringExtra(EXTRA_KEY)
-                }
-
-            }
+            findNavController().navigate(R.id.action_a_to_b)
         }
 
-    private fun fetchResultFromB() {
-        val intent = Intent(this, B::class.java)
-        startActivityForResult(intent, CODE)
+        tvOld.text = args.oldData
+        tvNew.text = args.newData
     }
 
-    private fun fetchResultFromBWithNewStrategy() {
-        val intent = Intent(this, B::class.java)
-        resultFromB.launch(intent)
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == CODE && resultCode == Activity.RESULT_OK) {
-
-            data?.let {
-                tvOld.text = data.getStringExtra(EXTRA_KEY)
-            }
-
-        }
-    }
 }
---
+++ app/src/main/java/com/galaxytechno/activityresultcode/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.galaxytechno.activityresultcode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host) as NavHostFragment
+        //navController for destination control
+        navController = navHostFragment.navController
+        //appBarConfig for appBar control
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setup graph with appbar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
